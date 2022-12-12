c5ac014e1fcdbb3d166f2a1efa2f83c2715fcfcd
Jack <98179941+peterthecoder1234@users.noreply.github.com>
2022-12-11 21:23:45 -0500

    new file: components/Layout.tsx, modified: components/Navbar.tsx, modified: pages/_app.tsx, modified: pages/index.tsx, modified: styles/globals.css

--- website/components/Navbar.tsx
+++ website/components/Navbar.tsx
@@ -3,25 +3,29 @@ import Link from "next/link"
 const Navbar = () =>
 {
   return (
-    <nav className = "flex justify-center flex-wrap bg-gray-800 p-6 shadow-blue-400 w-screen">
+    <nav className = "flex justify-center flex-wrap bg-gray-900 p-6 w-screen ">
+
       <div className = "w-full block flex-grow sm:flex sm:items-center sm:w-auto">
-        <div className = "flex items-center flex-shrink-0 mr-6">
-          <h1 className = "text-white text-2xl font-bold">Gratifying Games</h1>
+        <div className = "flex items-center flex-shrink-0 mr-10">
+          <h1 className = "text-red-400 text-2xl font-bold">Gratifying Games</h1>
         </div>
 
         {/* MAIN Navagation */}
         <div className = "text-sm sm:flex-grow">
-          <Link href = "/" className = "block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white pl-2 pr-2">Home</Link>
-          <Link href = "/" className = "block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white pl-2 pr-2">Games</Link>
-          <Link href = "/" className = "block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white pl-2 pr-2">About</Link>
-          <Link href = "/" className = "block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white pl-2 pr-2">Blog</Link>
+
+          <Link href = "/" className = "block mt-4 sm:inline-block sm:mt-0 text-white hover:text-red-400 pl-2 pr-2">Home</Link>
+          <Link href = "/" className = "block mt-4 sm:inline-block sm:mt-0 text-white hover:text-red-400 pl-2 pr-2">Games</Link>
+          <Link href = "/about" className = "block mt-4 sm:inline-block sm:mt-0 text-white hover:text-red-400 pl-2 pr-2">About</Link>
+          <Link href = "/" className = "block mt-4 sm:inline-block sm:mt-0 text-white hover:text-red-400 pl-2 pr-2">Blog</Link>
+
         </div>  
 
         {/* SEARCH BAR */}
-        <div className = "sm:inline-block flex-col mt-4 md:mt-0  py-3 w-full sm:w-1/3">
-          <input className = "p-3 rounded-xl text-center text-teal-200 bg-gray-700 w-full " type = "text" placeholder="Search"></input>
+        <div className = "sm:inline-block flex-col mt-4 md:mt-0  py-3 w-full sm:w-1/3 mr-10">
+          <input className = "p-3 rounded-xl text-center text-red-400 bg-gray-800 w-full " type = "text" placeholder="Search"></input>
         </div>
       </div>
+
     </nav>
   )
 }
--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,9 +1,32 @@
 import Navbar from "../components/Navbar"
+import Link from "next/link"
+import Head from "next/head"
 
 export default function Home() {
   return (
     <>
-      <Navbar />
+      {/* HEAD */}
+      <Head>
+        <title>Home</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+      </Head>
+
+      <section className = "h-screen bg-gray-900 flex justify-center  ">
+        <div className = "mt-20 rounded-xl  flex bg-cover justify-center text-center items-center w-3/4 h-3/4 flex-col bg-[url('https://images.hdqwalls.com/wallpapers/planet-space-art-ka.jpg')]">
+          <span className = "text-4xl text-white font-bold ">Dive into Ball Racers</span>
+
+          <Link href = "#" className = " font-bold pr-12 pl-12 pt-4 pb-4 bg-red-400 rounded-3xl mt-10 hover:  text-white">Play Now</Link>
+        </div>
+      </section>
+
+      <section className = "h-screen bg-slate-200 flex justify-center items-center">
+        <div className = "rounded-xl flex bg-cover justify-center items-center text-center w-3/4 h-3/4 flex-col bg-[url('https://i.pinimg.com/originals/d3/cf/a6/d3cfa6dea2ac714d8c5d7b865459f7d3.jpg')]">
+          <span className = "text-4xl text-white font-bold">Dive into Ball Bonkers</span>
+
+          <Link href = "#" className = " font-bold pr-12 pl-12 pt-4 pb-4 bg-white rounded-3xl mt-10 hover:  text-black">Play Now</Link>
+        </div>
+      </section>
     </>
   )
 }
