4ba0ed350be4dd46aa56a3814740db2078992dfe
Jack <98179941+peterthecoder1234@users.noreply.github.com>
2022-12-12 12:39:14 -0500

    modified: pages/about.tsx

--- website/pages/about.tsx
+++ website/pages/about.tsx
@@ -2,8 +2,20 @@ const about = () =>
 {
   return (
     <>
-      <main>
-        <span>About</span>
+      <main className = "bg-gray-900">
+        <header className = "flex justify-center">
+          <h1 className = "text-white text-3xl font-bold">About</h1>
+        </header>
+
+        <section className = "flex justify-row h-screen items-center space-x-6">
+          <div className = "w-1/2 h-1/2 bg-gray-600 border border-red-400 rounded-xl ml-5 text-center">
+            <span className = "text-white text-2xl font-bold">The Orgin</span>
+
+            <p></p>
+          </div>
+          <div className = "w-1/2 h-1/2 bg-gray-600 border border-red-400 rounded-xl"></div>
+          <div className = "w-1/2 h-1/2 bg-gray-600 border border-red-400 rounded-xl mr-5"></div>
+        </section>
       </main>
     </>
   )
--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 
       <section className = "h-screen bg-gray-900 flex justify-center  ">
         <div className = "mt-20 rounded-xl  flex bg-cover justify-center text-center items-center w-5/6 h-3/4 flex-col bg-[url('https://images.hdqwalls.com/wallpapers/planet-space-art-ka.jpg')]">
-          <span className = "text-4xl text-white font-bold ">Dive into Ball Racers</span>
+          <span className = "text-4xl text-white font-bold ">Race into Ball Racers</span>
           <p className = "text-white text-l w-1/2 mt-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati aperiam rem quibusdam, eos ratione dicta!</p>
 
           <Link href = "#" className = " font-bold pr-12 pl-12 pt-4 pb-4 hover:bg-white hover:text-red-400 bg-red-400 rounded-3xl mt-10 hover:  text-white">Play Now</Link>
@@ -23,7 +23,7 @@ export default function Home() {
 
       <section className = "h-screen bg-white flex justify-center items-center">
         <div className = "rounded-xl flex bg-cover justify-center items-center text-center w-5/6 h-3/4 flex-col bg-[url('https://4kwallpapers.com/images/walls/thumbs_3t/3792.jpg')]">
-          <span className = "text-4xl text-white outline-8  font-bold">Dive into Ball Bonkers</span>
+          <span className = "text-4xl text-white outline-8  font-bold">Roll into Ball Bonkers</span>
           <p className = "text-white text-l w-1/2 mt-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, esse tenetur sapiente amet quos dolorem?</p>
 
           <Link href = "#" className = " font-bold pr-12 pl-12 pt-4 pb-4 bg-white rounded-3xl mt-10 text-emerald-200 hover:bg-emerald-300 hover:text-white">Play Now</Link>
