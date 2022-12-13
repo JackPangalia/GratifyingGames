b977c6759563b0ce42b6313bf095626d731de0a9
Mose Run <moserun@Moses-MacBook-Air.local>
2022-12-12 20:03:27 -0500

    modified: pages/about.tsx

--- website/pages/about.tsx
+++ website/pages/about.tsx
@@ -4,17 +4,27 @@ const about = () =>
     <>
       <main className = "bg-gray-900">
         <header className = "flex justify-center">
-          <h1 className = "text-white text-3xl font-bold">About</h1>
+          <h1 className = "text-white text-4xl mt-4 font-bold">About</h1>
         </header>
 
-        <section className = "flex justify-row h-screen items-center space-x-6">
-          <div className = "w-1/2 h-1/2 bg-gray-600 border border-red-400 rounded-xl ml-5 text-center">
-            <span className = "text-white text-2xl font-bold">The Orgin</span>
+        <section className = "flex h-screen mt-20 flex-col items-center">
+          <div className = "ml-10  w-1/2 h-1/2 bg-gray-800 border border-red-400 rounded-xl flex justify-center items-center flex-col text-center">
+            <span className = "text-red-400 text-2xl font-bold">The Orgin</span>
 
-            <p></p>
+            <p className = "mt-5 w-4/5 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat tempora similique adipisci illo corporis eius!</p>
           </div>
-          <div className = "w-1/2 h-1/2 bg-gray-600 border border-red-400 rounded-xl"></div>
-          <div className = "w-1/2 h-1/2 bg-gray-600 border border-red-400 rounded-xl mr-5"></div>
+
+          <div className = "mr-10 ml-10 w-1/2 h-1/2 bg-gray-800 border border-red-400 rounded-xl flex justify-center items-center flex-col text-center">
+            <span className = "text-red-400 text-2xl font-bold">Our Plans</span>
+
+            <p className = "mt-5 w-4/5 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat tempora similique adipisci illo corporis eius!</p>
+          </div>
+
+          <div className = "mr-10 w-1/2 h-1/2 bg-gray-800 border border-red-400 rounded-xl flex justify-center items-center flex-col text-center ">
+            <span className = "text-red-400 text-2xl font-bold">What We Do</span>
+
+            <p className = "mt-5 w-4/5 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat tempora similique adipisci illo corporis eius!</p>
+          </div>
         </section>
       </main>
     </>
