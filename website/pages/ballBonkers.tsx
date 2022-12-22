51d252798a80e0c18b498765acf4ca136dff7f87
Mose Run <moserun@Moses-MacBook-Air.local>
2022-12-22 18:41:55 -0500

    changed ball bonkers.tsx

--- website/pages/ballBonkers.tsx
+++ website/pages/ballBonkers.tsx
@@ -11,36 +11,19 @@ const ballBonkersDownload = () => {
         <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
       </Head>
 
-      <main className = "bg-white h-screen">
-        <section className = "w-full lg:h-2/3 h-full bg-[url('https://4kwallpapers.com/images/walls/thumbs_3t/3792.jpg')] bg-cover font-bold flex items-center justify-center ">
-          <div className = "flex flex-col text-center items-center lg:absolute lg:left-40 mr-5 ml-5">
-            <p className = "text-white font-semibold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, saepe. Fugiat, qui ab.</p>
-            <div className = "bg-gray-900 pr-10 pl-10 pt-5 pb-5 mt-8 rounded-xl">
-            <Link href = "#" className = " text-white font-semibold text-3xl bg-clip-text text-transparent bg-gradient-to-r from-red-400 to-white rounded-2xl">Download</Link>
+      <main className = "h-screen bg-gray-900 space-y-20">
+        <header className = "flex justify-center items-center flex-col text-center">
+          <h1 className = "mt-20 text-5xl bg-clip-text text-transparent bg-gradient-to-r from-red-400 to-white font-bold">Download Ball Bonkers</h1>
+          <p className = "text-white font-semibold w-1/3 text-xl">Lorem . Commodi inventore igendi Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quas ratione eos.</p>
+        </header>
 
-            </div>
+        <div className = "h-1/2 bg-gray-200 rounded-lg flex justify-center items-center">
+          <div className = "w-1/3 h-20 text- rounded-xl bg-gray-900">
+           <Link href = "#" className = "text-4xl bg-gradient-to-r from-red-400 to-red-300 bg-clip-text text-transparent font-semibold ">Play Now</Link>
+
           </div>
+        </div>
 
-  
-        </section>
-
-        <section className = "w-full lg:h-1/2 h-full flex flex-col lg:flex-row bg-gray-900">
-          <div className = "sm:mt-0 mt-10 h-full lg:w-1/2 text-center flex flex-col justify-center p-10">
-            <span className = "text-2xl font-semibold text-red-400 overline">The Game</span>
-            <p className = "text-l text-gray-400 mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nam voluptas aspernatur eveniet. Nostrum animi soluta ex aliquid, nemo delectus Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus iste sapiente excepturi..</p>
-          </div>
-
-          <div className = "h-full lg:w-1/2 text-center flex flex-col justify-center p-10">
-            <span className = "text-2xl font-semibold text-red-400 overline">Features</span>
-            <p className = "text-l text-gray-400 mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nam voluptas aspernatur eveniet. Nostrum animi soluta ex aliquid, nemo delectus.</p>
-          </div>
-
-          <div className = "h-full lg:w-1/2 text-center flex flex-col justify-center p-10">
-            <span className = "text-2xl font-semibold text-red-400 overline ">Content</span>
-            <p className = "text-l text-gray-400 mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nam voluptas aspernatur eveniet. Nostrum animi soluta ex aliquid, nemo delectus.</p>
-          </div>
-  
-        </section>
       </main>
     </>
   )
